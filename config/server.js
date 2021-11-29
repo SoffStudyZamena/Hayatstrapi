83f89dceb4ec28daffab7406340bf6c964949b9e
Erkin303m <maxmudjonoverkin189@gmail.com>
2021-11-29 09:39:49 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd069dac2d165a8d990feddd784dc7eb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e4f5cc52-96ca-4f2f-a1b3-76bb5d38f180'
+};
